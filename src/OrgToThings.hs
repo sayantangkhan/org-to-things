7334081a3694149a63052dd882c01aac92c5afca
Sayantan Khan <sayantangkhan@gmail.com>
2022-08-07 15:32:19 -0400

    Need to add parser for RawBlocks

--- src/OrgToThings.hs
+++ src/OrgToThings.hs
@@ -6,6 +6,7 @@ module OrgToThings
 where
 
 import Control.Monad.Error.Class (MonadError (throwError))
+import Control.Monad.IO.Class
 import qualified Data.Text.IO as TIO
 import OrgToThings.Parser (parseAndTransformBlocks)
 import Safe (headMay)
@@ -26,6 +27,7 @@ mainFunc = do
       result <- runIO $ do
         agenda <- readOrg def agendaSource
         let parsedAndTransformed = parseAndTransformBlocks agenda
+        liftIO $ putStrLn $ show parsedAndTransformed
         case parsedAndTransformed of
           Left _ -> writeOrg def agenda
           Right transformedAgenda -> writeOrg def transformedAgenda
